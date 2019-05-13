84206bdbd92c83a0d35c4e3e8a6e56f665936ecd
Alex Goller <alpipego@protonmail.com>
2019-05-13 12:15:47 +0200

    check for customizer

--- wield-menu.php
+++ wield-menu.php
@@ -10,9 +10,16 @@
  * License: MIT
  */
 
-if ( version_compare( PHP_VERSION, '5.4.0' ) >= 0 && function_exists('wp_add_inline_script')) {
-	add_action( 'admin_enqueue_scripts', function () {
-		wp_add_inline_script( 'nav-menu', file_get_contents( __DIR__ . '/js/wield-menu.js' ) );
-		wp_add_inline_style( 'nav-menus', file_get_contents( __DIR__ . '/css/wield-menu.css' ) );
-	} );
+if ( version_compare( PHP_VERSION, '5.4.0' ) >= 0 && function_exists( 'wp_add_inline_script' ) ) {
+    add_action( 'admin_enqueue_scripts', function () {
+        $min = defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ? '' : '.min';
+        wp_add_inline_script(
+            is_customize_preview() ? 'customize-nav-menus' : 'nav-menu',
+            file_get_contents( __DIR__ . '/js/wield-menu' . $min . '.js' )
+        );
+        wp_add_inline_style(
+            is_customize_preview() ? 'customize-nav-menus' : 'nav-menus',
+            file_get_contents( __DIR__ . '/css/wield-menu' . $min . '.css' )
+        );
+    } );
 }
