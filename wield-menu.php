5c9a31bc9312d7b3ecb4f7f3ff03d5d4e49bd619
Alex Goller <alpipego@protonmail.com>
2019-05-13 09:38:49 +0200

    make sure inline_script function exists

--- wield-menu.php
+++ wield-menu.php
@@ -10,7 +10,7 @@
  * License: MIT
  */
 
-if ( version_compare( PHP_VERSION, '5.4.0' ) >= 0 ) {
+if ( version_compare( PHP_VERSION, '5.4.0' ) >= 0 && function_exists('wp_add_inline_script')) {
 	add_action( 'admin_enqueue_scripts', function () {
 		wp_add_inline_script( 'nav-menu', file_get_contents( __DIR__ . '/js/wield-menu.js' ) );
 		wp_add_inline_style( 'nav-menus', file_get_contents( __DIR__ . '/css/wield-menu.css' ) );
